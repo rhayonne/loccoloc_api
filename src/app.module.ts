c5554ecdc677bb605a282678ad599692d7157c93
Rhayonne Lopes de Almeida <rhayonne@gmail.com>
2025-11-17 21:40:29 +0100

    Start App; Auth jsut created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}.local`,
+        `.env.${process.env.NODE_ENV}`,
+      ],
+    }),
+    MongooseModule.forRoot(`${process.env.MONGO_URI}`),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
